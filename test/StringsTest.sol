6e9538194f376a5a46a67a6853f475ba091a25ff
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-07-20 17:41:55 +0700

    add test case

--- test/StringsTest.sol
+++ test/StringsTest.sol
@@ -1,11 +1,37 @@
 pragma solidity ^0.4.16;
 
-import "../lib/Integers.sol";
 import "../lib/Strings.sol";
 
 contract StringsTest {
-    using Integers for uint;
+
     using Strings for string;
+    
+    function testLength() public {
+        string memory myStr = "20scoops CNX";
+        assert(12 == myStr.length());
+    }
 
+    function testConcat() public {
+        string memory result = "20scoops CNX";
+        string memory str = "20scoops";
+        string memory expected = str.concat(" ").concat("CNX");
+        assert(expected.compareTo(result));
+    }
 
+    function testReplace() public {
+        string memory str = "20scoops CNX ";
+        string memory expected = "20scoopsaCNX ";
+        assert(expected.compareTo(str.replace(" ","a")));
+    }
+
+    function testReplaceAll() public {
+        string memory str = "20scoops CNX ";
+        string memory expected = "20scoopsaCNXa";
+        assert(expected.compareTo(str.replaceAll(" ","a")));
+    }
+
+    function testCompareTo() public {
+        string memory str = "20scoops CNX";
+        assert(str.compareTo("20scoops CNX"));
+    }
 }
